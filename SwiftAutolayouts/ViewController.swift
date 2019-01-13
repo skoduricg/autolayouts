e05e881ba543e559321c7b0255da9faba002feec
skoduri <skoduri@charterglobal.com>
2019-01-13 16:39:37 +0530

    Initial Commit

---
+++ SwiftAutolayouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAutolayouts
+//
+//  Created by Satish Koduri on 13/01/19.
+//  Copyright © 2019 Satish Koduri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
